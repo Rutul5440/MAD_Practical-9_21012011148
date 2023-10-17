63bd72442209de47d94b52c62fc2c4aba946797b
student <student@gnu.ac.in>
2023-10-17 08:14:32 +0530

    commited

--- app/src/main/java/com/example/mad_practical_9_21012011148/SplashActivity.kt
+++ app/src/main/java/com/example/mad_practical_9_21012011148/SplashActivity.kt
@@ -3,33 +3,48 @@ package com.example.mad_practical_9_21012011148
 import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.Animation.AnimationListener
+import android.view.animation.AnimationUtils
 import android.widget.ImageView
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity(),AnimationListener {
 
     lateinit var imageAnimation : AnimationDrawable
     lateinit var logoImage : ImageView
+    lateinit var logoanimation : Animation
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        val imageUvpce = findViewById<ImageView>(R.id.imageuvpce)
-        imageUvpce.setBackgroundResource(R.drawable.logo_animation_list)
-        imageAnimation = imageUvpce.background as AnimationDrawable
-
-
-
+        logoImage = findViewById(R.id.imageuvpce)
+        logoImage.setBackgroundResource(R.drawable.logo_animation_list)
+        imageAnimation = logoImage.background as AnimationDrawable
+        logoanimation = AnimationUtils.loadAnimation(this,R.anim.twin_animation)
+        logoanimation.setAnimationListener(this)
     }
 
     override fun onWindowFocusChanged(hasFocus: Boolean) {
         if(hasFocus){
             imageAnimation.start()
-            logoImage.startAnimation(logoAnimation)
+            logoImage.startAnimation(logoanimation)
         }
         else{
             imageAnimation.stop()
         }
         super.onWindowFocusChanged(hasFocus)
+    }
+
+    override fun onAnimationStart(p0: Animation?) {
+
+    }
+
+    override fun onAnimationEnd(p0: Animation?) {
+
+    }
+
+    override fun onAnimationRepeat(p0: Animation?) {
+
     }
 }
